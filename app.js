6db5311032dc4dcccae6daf583bfa0a718fbf957
Owen Medina <owensmedi@gmail.com>
2021-04-15 18:59:54 -0700

    Add basic layout for timeline page

--- app.js
+++ app.js
@@ -3,11 +3,14 @@
 const express = require("express");
 
 const app = express();
+app.set("view engine", "ejs");
+app.use(express.static("public"));
 
-const port = parseInt(process.env.PORT) || 3000;
+const port = process.env.PORT || 3000;
 
 app.get("/", function (req, res) {
   console.log("Some TimeController function");
+  res.render("timeline");
 });
 
 app.listen(port, function () {
